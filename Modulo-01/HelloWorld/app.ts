06a35828c5ad30abd024cff3d6cf99e037cf711b
RHamudi <ramonramos.silva16@gmail.com>
2022-09-09 07:45:36 -0300

    att

--- Modulo-01/HelloWorld/app.ts
+++ Modulo-01/HelloWorld/app.ts
@@ -1,3 +1,10 @@
-let listaFruta: [string, ... string[]] = ['laranja', 'pera', 'maça', 'banana']
+/**
+ * arquivo: helloworld/app.ts
+ * descrição: arquivo responsável por ensinar conceitos básicos sobre TypeScript
+ * data: 16/02/2021
+ * author: Glaucia Lemos <@glaucia_lemos86>
+ * Transpilação do arquivo: <Windows> CTRL + SHIFT + B
+ */
 
-console.log(... listaFruta)
+let mensagem: string = 'Hello World, Glaucia Lemos!';
+console.log(mensagem);
